cd085743bcbb0d2dc451553f5159f86cae5268e3
amir-hamjah <amirhamjah.laam@gmail.com>
2020-08-09 13:44:24 +0600

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,12 @@
+  $(document).ready(function(){
+        
+    $('.venobox_custom').venobox({
+        framewidth: '400px',        // default: ''
+        frameheight: '300px',       // default: ''
+        border: '10px',             // default: '0'
+        bgcolor: '#5dff5e',         // default: '#fff'
+        titleattr: 'data-title',    // default: 'title'
+        numeratio: true,            // default: false
+        infinigall: true            // default: false
+    });
+});
